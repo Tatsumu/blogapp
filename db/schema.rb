ad01257fc64293231551b7a8af61c7e07ef72a34
tatsumu <t.m.555.tennis@gmail.com>
2015-09-29 22:42:41 +0900

    error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150927144041) do
+ActiveRecord::Schema.define(version: 20150929125717) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body"
+    t.boolean  "done",       default: false
+    t.integer  "post_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
