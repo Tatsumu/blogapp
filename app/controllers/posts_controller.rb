c0fe66eb1276cbe71a18f2f814b48481b690fa7f
tatsumu <t.m.555.tennis@gmail.com>
2015-09-29 21:30:45 +0900

    11th commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+
+	before_action :set_project, only:[:show, :edit, :update, :destroy]
+
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -21,11 +23,9 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = Post.find(params[:id])
 		if @post.update(project_params)
 			redirect_to posts_path
 		else
@@ -34,7 +34,6 @@ class PostsController < ApplicationController
 	end
 
 	def destroy
-		@post = Post.find(params[:id])
 		@post.destroy
 		redirect_to posts_path
 	end
@@ -43,6 +42,10 @@ class PostsController < ApplicationController
 
 		def project_params
 			params[:post].permit(:title, :description)
+		end
+
+		def set_project
+			@post = Post.find(params[:id])
 		end
 end
 
