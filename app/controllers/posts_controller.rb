e8e0fbf573db4f85483afd5c5fb71bd7284cdef1
tatsumu <t.m.555.tennis@gmail.com>
2015-09-29 20:45:20 +0900

    6th commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,20 @@ class PostsController < ApplicationController
 	def show
 		@post = Post.find(params[:id])
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post =  Post.new(project_params)
+		@post.save
+		redirect_to posts_path
+	end
+
+	private
+
+		def project_params
+			params[:post].permit(:title, :description)
+		end
 end
